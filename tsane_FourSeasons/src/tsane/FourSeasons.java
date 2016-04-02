f15ba7149faae02fcad97faae66bd157f81ec3d6
Tanuj Sane <tsane@wpi.edu>
2016-04-02 02:03:25 -0400

    GUI coordinates calibrated

--- tsane_FourSeasons/src/tsane/FourSeasons.java
+++ tsane_FourSeasons/src/tsane/FourSeasons.java
@@ -98,39 +98,39 @@ public class FourSeasons extends Solitaire {
 		addViewWidget(wasteView);
 		
 		heartFView = new PileView(heartF);
-		heartFView.setBounds(178+ci.getWidth(), 30, ci.getWidth(), ci.getHeight());
+		heartFView.setBounds(100+ci.getWidth(), 30, ci.getWidth(), ci.getHeight());
 		addViewWidget(heartFView);
 		
 		spadeFView = new PileView(spadeF);
-		spadeFView.setBounds(626+2*ci.getWidth(), 30, ci.getWidth(), ci.getHeight());
+		spadeFView.setBounds(450+2*ci.getWidth(), 30, ci.getWidth(), ci.getHeight());
 		addViewWidget(spadeFView);
 		
 		clubFView = new PileView(clubF);
-		clubFView.setBounds(178+ci.getWidth(), 241+ci.getHeight(), ci.getWidth(), ci.getHeight());
+		clubFView.setBounds(100+ci.getWidth(), 271+ci.getHeight(), ci.getWidth(), ci.getHeight());
 		addViewWidget(clubFView);
 		
 		diamondFView = new PileView(diamondF);
-		diamondFView.setBounds(626+2*ci.getWidth(), 241+ci.getHeight(), ci.getWidth(), ci.getHeight());
+		diamondFView.setBounds(450+2*ci.getWidth(), 271+ci.getHeight(), ci.getWidth(), ci.getHeight());
 		addViewWidget(diamondFView);
 		
 		crossLeftView = new PileView(crossLeft);
-		crossLeftView.setBounds(466, 57+ci.getHeight(), ci.getWidth(), ci.getHeight());
+		crossLeftView.setBounds(139+2*ci.getWidth(), 87+ci.getHeight(), ci.getWidth(), ci.getHeight());
 		addViewWidget(crossLeftView);
 		
 		crossMidView = new PileView(crossMid);
-		crossMidView.setBounds(466+ci.getWidth(), 57+ci.getHeight(), ci.getWidth(), ci.getHeight());
+		crossMidView.setBounds(159+3*ci.getWidth(), 87+ci.getHeight(), ci.getWidth(), ci.getHeight());
 		addViewWidget(crossMidView);
 		
 		crossRightView = new PileView(crossRight);
-		crossRightView.setBounds(466+2*ci.getWidth(), 57+ci.getHeight(), ci.getWidth(), ci.getHeight());
+		crossRightView.setBounds(179+4*ci.getWidth(), 87+ci.getHeight(), ci.getWidth(), ci.getHeight());
 		addViewWidget(crossRightView);
 		
 		crossTopView = new PileView(crossTop);
-		crossTopView.setBounds(466+ci.getWidth(), 57, ci.getWidth(), ci.getHeight());
+		crossTopView.setBounds(159+3*ci.getWidth(), 67, ci.getWidth(), ci.getHeight());
 		addViewWidget(crossTopView);
 		
 		crossBottomView = new PileView(crossBottom);
-		crossBottomView.setBounds(466+2*ci.getWidth(), 57+2*ci.getHeight(), ci.getWidth(), ci.getHeight());
+		crossBottomView.setBounds(159+3*ci.getWidth(), 107+2*ci.getHeight(), ci.getWidth(), ci.getHeight());
 		addViewWidget(crossBottomView);
 	}
 	
